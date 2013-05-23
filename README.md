c834683571012139c77761528dba884088e351df
Lucas Guimarães <lucas@lucasguimaraes.com>
2013-05-23 13:59:58 -0300

    First Commit
    Sending files for first time

---
+++ README.md
@@ -0,0 +1,10 @@
+Scrolling Marquee
+=
+A simple VB.NET scrolling marquee for use as a component in .NET projects.
+
+Original by http://www.vbforums.com/member.php?109588-NetNinja
+Upgraded by Lucas Guimarães (23/05/2013) - http://lucasguimaraes.com <contato@lucasguimaraes.com>
+
+
+=
+Scrolling Marquee needs .NET Framework 3.0 and Visual Studio 2012 or newer.
---
+++ ScrollingMarquee/My Project/AssemblyInfo.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Scrolling Marquee")> 
+<Assembly: AssemblyDescription("Scroll a text over the screen.")> 
+<Assembly: AssemblyCompany("LucasGuimaraes.com")> 
+<Assembly: AssemblyProduct("Scrolling Marquee Control")> 
+<Assembly: AssemblyCopyright("Copyright © 2013 LucasGuimaraes.com")> 
+<Assembly: AssemblyTrademark("LucasGuimaraes.com")> 
+
+<Assembly: ComVisible(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("cbfd1b3a-ba8c-1b60-b306-6ce7ca993a5f")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("en")> 
